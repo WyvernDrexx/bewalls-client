fb17a5ca78829d75512bb17447403b73a21c59d1
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-05-21 18:48:56 +0530

    @redux/toolkit setup

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { RootNavigator } from './src/navigation';
+import { store } from './src/store';
 
 function App() {
-  return <RootNavigator />;
+  return (
+    <Provider store={store}>
+      <RootNavigator />
+    </Provider>
+  );
 }
 
 export default App;
