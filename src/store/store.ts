c6411e46f8fd1fa5c8c20a8fcc2e1edc7c4facaf
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-06-02 20:42:37 +0530

    move  selector and dispatch hooks to store file

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import themeReducer from './theme';
 
@@ -12,3 +13,6 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
