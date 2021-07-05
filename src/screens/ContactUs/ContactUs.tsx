fb1d64d7828e11a2161b531deeda966b866fff89
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-07-05 08:40:53 +0530

    contact us page design

--- src/screens/ContactUs/ContactUs.tsx
+++ src/screens/ContactUs/ContactUs.tsx
@@ -1,22 +1,65 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import StackHeader from '../../components/StackHeader';
 import { useTheme } from '../../hooks';
 import { ContactUsScreenProps } from '../../navigation/types';
+import { hp, wp } from '../../utilities';
 
 const ContactUs: React.FC<ContactUsScreenProps> = function () {
   const { themedStyles } = useTheme();
 
   return (
-    <View style={[styles.root, themedStyles.bg]}>
+    <>
       <StackHeader title="Contact Us" />
-    </View>
+
+      <View style={[styles.root, themedStyles.bg]}>
+        <View style={styles.container}>
+          <Text style={[styles.heading]}>Content Manager</Text>
+          <Text style={[styles.text]}>
+            If you have any queries related with our contents you can contact us
+            at
+          </Text>
+          <Text style={styles.contactEmail}>deepanjan@androcrunch.com</Text>
+          <Text style={[styles.heading, styles.devContactHeader]}>
+            Developer Contact
+          </Text>
+          <Text style={[styles.text]}>
+            For any technical guidance realted to this app, you can contact the
+            developer at
+          </Text>
+          <Text style={styles.contactEmail}>lp@androcrunch.com</Text>
+        </View>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  container: {
+    paddingHorizontal: wp(2),
+    height: hp(20),
+  },
+  contactEmail: {
+    backgroundColor: 'black',
+    width: wp(50),
+    borderRadius: wp(1),
+    color: 'white',
+    padding: wp(2),
+    marginLeft: wp(25),
+    textAlign: 'center',
+  },
+  heading: {
+    fontSize: wp(4),
+    fontWeight: 'bold',
+  },
+  text: {
+    marginVertical: hp(2),
+  },
+  devContactHeader: {
+    marginTop: hp(2),
   },
 });
 
