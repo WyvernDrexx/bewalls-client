33ecc03990d6764176136fa6e183d630dc445c54
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-07-05 08:13:15 +0530

    organise imports

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,4 +1,4 @@
 export * from './dimensionUtils';
 export * from './isLastElement';
-export * from './verifyUserCreateInput';
 export * from './numberToMetricScale';
+export * from './verifyUserCreateInput';
