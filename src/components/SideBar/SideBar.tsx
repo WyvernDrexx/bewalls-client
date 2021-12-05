0cd10e9cfe9cdb60c872571da5e7b85df1aea893
WyvernDrexx <rajsharmaof@gmail.com>
2021-12-05 18:42:41 +0530

    [#ID] sidebar icon change

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,172 +1,148 @@
-import React, { useEffect, useState } from 'react';
-import {
-  Image,
-  StyleProp,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ViewStyle,
-} from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import Animated, {
-  interpolate,
-  runOnJS,
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
-import { SvgProps } from 'react-native-svg';
-import { useLocal, useTheme, useUser } from '../../hooks';
-import { RootStackParamList } from '../../navigation/types';
-import { hp, wp } from '../../utilities';
-import BackSvg from './back.svg';
-import BarItem from './BarItem';
-import CategoriesSvg from './categories.svg';
-import ContactUsSvg from './envelope.svg';
-import FavSvg from './heart.svg';
-import HomeSvg from './home.svg';
-import ProfileSvg from './profile.svg';
-import SlidersSvg from './sliders.svg';
-import LoginNowSvg from './login-now.svg';
+import React, { useEffect, useState } from 'react'
+import { Image, StyleProp, StyleSheet, Text, TouchableOpacity, View, ViewStyle } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
+import Animated, { interpolate, runOnJS, useAnimatedStyle, useSharedValue } from 'react-native-reanimated'
+import { SvgProps } from 'react-native-svg'
+import Icon from 'react-native-vector-icons/Ionicons'
+import MUIIcons from 'react-native-vector-icons/MaterialIcons'
+import { useLocal, useTheme, useUser } from '../../hooks'
+import { RootStackParamList } from '../../navigation/types'
+import { hp, wp } from '../../utilities'
+import BackSvg from './back.svg'
+import BarItem from './BarItem'
+import ContactUsSvg from './envelope.svg'
+import FavSvg from './heart.svg'
+import ProfileSvg from './profile.svg'
+import SlidersSvg from './sliders.svg'
+
 
 type SideBarProps = {
-  isShown?: boolean;
-  animatedStyle?: StyleProp<ViewStyle>;
-  onClose?: () => void;
-  onItemClick: (route: keyof RootStackParamList) => void;
-  currentRoute: keyof RootStackParamList;
-};
+  isShown?: boolean
+  animatedStyle?: StyleProp<ViewStyle>
+  onClose?: () => void
+  onItemClick: (route: keyof RootStackParamList) => void
+  currentRoute: keyof RootStackParamList
+}
 
 type BarItemType = {
-  route: keyof RootStackParamList;
-  title: string;
-  hideWhenLoggedIn?: boolean;
-  showWhenLoggedIn?: boolean;
-  icon: React.FC<SvgProps>;
-};
+  route: keyof RootStackParamList
+  title: string
+  hideWhenLoggedIn?: boolean
+  showWhenLoggedIn?: boolean
+  icon: React.FC<SvgProps>
+}
 
 const SIDEBAR_ITEMS: BarItemType[] = [
   {
     route: 'Home',
     title: 'Home',
-    icon: HomeSvg,
+    icon: () => <Icon name='home-sharp' size={wp(6)} />
   },
   {
     route: 'Profile',
     title: 'Profile',
-    icon: LoginNowSvg,
-    showWhenLoggedIn: true,
+    icon: () => <Icon name='person' size={wp(6)} />,
+    showWhenLoggedIn: true
   },
   {
     route: 'Categories',
     title: 'Categories',
-    icon: CategoriesSvg,
+    icon: () => <MUIIcons name='category' size={wp(6)} />
   },
   {
     route: 'Favourites',
     title: 'Favourites',
     icon: FavSvg,
-    showWhenLoggedIn: true,
+    showWhenLoggedIn: true
   },
   {
     route: 'SignIn',
     title: 'Sign In/Sign Up',
     hideWhenLoggedIn: true,
-    icon: ProfileSvg,
+    icon: ProfileSvg
   },
   {
     route: 'Settings',
     title: 'Settings',
-    icon: SlidersSvg,
+    icon: () => <Icon name='settings' size={wp(6)} />
   },
   {
     route: 'ContactUs',
     title: 'Contact Us',
-    icon: ContactUsSvg,
-  },
-];
+    icon: () => <Icon name='ios-mail' size={wp(6)} />
+  }
+]
 
 const SideBar: React.FC<SideBarProps> = function (props) {
-  const width = wp(100);
-  const isShown = useSharedValue(props.isShown);
-  const user = useUser();
-  const { profileImageUri } = useLocal();
-  const [toCall, setToCall] = useState<'close' | 'barItem' | null>(null);
+  const width = wp(100)
+  const isShown = useSharedValue(props.isShown)
+  const user = useUser()
+  const { profileImageUri } = useLocal()
+  const [toCall, setToCall] = useState<'close' | 'barItem' | null>(null)
   const {
     themedStyles,
-    theme: { colors },
-  } = useTheme();
-  const [activeRoute, setActiveRoute] = useState<keyof RootStackParamList>();
+    theme: { colors }
+  } = useTheme()
+  const [activeRoute, setActiveRoute] = useState<keyof RootStackParamList>()
 
   useEffect(() => {
-    isShown.value = props.isShown;
-  }, [props.isShown, isShown]);
+    isShown.value = props.isShown
+  }, [props.isShown, isShown])
 
   const handleClose = () => {
-    setToCall('close');
-    isShown.value = false;
-  };
+    setToCall('close')
+    isShown.value = false
+  }
 
   const handleBarItemClick = (route: keyof RootStackParamList) => {
-    setToCall('barItem');
-    setActiveRoute(route);
-    isShown.value = false;
-  };
+    setToCall('barItem')
+    setActiveRoute(route)
+    isShown.value = false
+  }
 
   const handleAnimationComplete = (isFinished: boolean) => {
     // Handles the callback when the slide animation gets completed after close button is clicked
 
     if (isFinished && toCall) {
       if (props.onClose && toCall === 'close') {
-        props.onClose();
+        props.onClose()
       }
       if (toCall === 'barItem' && activeRoute) {
-        props.onItemClick(activeRoute);
+        props.onItemClick(activeRoute)
       }
-      setToCall(null);
+      setToCall(null)
     }
-  };
+  }
 
   const sideBarStyle = useAnimatedStyle(() => {
     const offsetX = interpolate(
       isShown.value === true ? 1 : 0, //Fastest way to convert boolean into int
       [0, 1],
-      [-width, 0],
-    );
+      [-width, 0]
+    )
 
     return {
       transform: [
         {
-          translateX: Animated.withTiming(
-            offsetX,
-            {},
-            runOnJS(handleAnimationComplete),
-          ),
-        },
-      ],
-    };
-  });
+          translateX: Animated.withTiming(offsetX, {}, runOnJS(handleAnimationComplete))
+        }
+      ]
+    }
+  })
 
   return (
     <Animated.View style={[sideBarStyle, styles.root, themedStyles.bg]}>
       <View style={[styles.sideBarItemsView]}>
-        <TouchableOpacity
-          style={[styles.backButton, { borderColor: colors.secondary }]}
-          onPress={handleClose}>
+        <TouchableOpacity style={[styles.backButton, { borderColor: colors.secondary }]} onPress={handleClose}>
           <BackSvg height={hp(2)} width={hp(2)} fill={colors.secondary} />
         </TouchableOpacity>
         <View style={[styles.profile]}>
           {profileImageUri ? (
-            <Image
-              style={styles.profileImage}
-              source={{ uri: profileImageUri }}
-            />
+            <Image style={styles.profileImage} source={{ uri: profileImageUri }} />
           ) : (
             <ProfileSvg height={hp(8)} width={hp(8)} fill={colors.secondary} />
           )}
-          <Text style={[styles.profileText, themedStyles.text]}>
-            {user.info ? user.info.fullName : 'You'}
-          </Text>
+          <Text style={[styles.profileText, themedStyles.text]}>{user.info ? user.info.fullName : 'You'}</Text>
         </View>
         <View style={[styles.barItems, { borderColor: colors.light }]}>
           {SIDEBAR_ITEMS.map((item, index) => {
@@ -181,7 +157,7 @@ const SideBar: React.FC<SideBarProps> = function (props) {
                 hideWhenLoggedIn={item.hideWhenLoggedIn}
                 showWhenLoggedIn={item.showWhenLoggedIn}
               />
-            );
+            )
           })}
         </View>
       </View>
@@ -189,8 +165,8 @@ const SideBar: React.FC<SideBarProps> = function (props) {
         <Animated.View style={[styles.transparentView]} />
       </TouchableWithoutFeedback>
     </Animated.View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   root: {
@@ -205,12 +181,12 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2,
+      height: 2
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
 
-    elevation: 5,
+    elevation: 5
   },
   backButton: {
     padding: wp(4),
@@ -218,37 +194,37 @@ const styles = StyleSheet.create({
     borderRadius: wp(50),
     position: 'absolute',
     right: wp(5),
-    top: hp(3),
+    top: hp(3)
   },
   sideBarItemsView: {
-    width: wp(75),
+    width: wp(75)
   },
   transparentView: {
     opacity: 0,
     width: wp(25),
     height: hp(100),
-    backgroundColor: 'black',
+    backgroundColor: 'black'
   },
   profile: {
     padding: wp(2),
-    marginTop: hp(10),
+    marginTop: hp(10)
   },
   profileText: {
     fontSize: wp(7),
     fontWeight: 'bold',
     marginTop: hp(3),
     color: 'white',
-    paddingBottom: hp(1),
+    paddingBottom: hp(1)
   },
   barItems: {
-    borderTopWidth: 1,
+    borderTopWidth: 1
   },
   profileImage: {
     borderRadius: wp(50),
     height: hp(8),
     width: hp(8),
-    backgroundColor: 'black',
-  },
-});
+    backgroundColor: 'black'
+  }
+})
 
-export default SideBar;
+export default SideBar
