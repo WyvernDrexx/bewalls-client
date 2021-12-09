642a29e7cc8eb3d8473157ffe0af52389be57678
WyvernDrexx <rajsharmaof@gmail.com>
2021-12-09 20:45:14 +0530

    [#180586299] selection screen header scroll with page

--- src/screens/Selection/Selection.tsx
+++ src/screens/Selection/Selection.tsx
@@ -1,105 +1,103 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Cards } from '../../components/Cards';
-import { LoadingView } from '../../components/Loader/LoadingView';
-import StackHeader from '../../components/StackHeader';
-import WallpaperView from '../../components/WallpaperView';
-import { useWallpapersQuery, Wallpaper } from '../../generated/graphql';
-import { useTheme, useWallpaperView } from '../../hooks';
-import { SelectionScreenProps } from '../../navigation/types';
-import { hp, wp } from '../../utilities';
+import React, { useCallback } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Cards } from '../../components/Cards'
+import { LoadingView } from '../../components/Loader/LoadingView'
+import StackHeader from '../../components/StackHeader'
+import WallpaperView from '../../components/WallpaperView'
+import { useWallpapersQuery, Wallpaper } from '../../generated/graphql'
+import { useTheme, useWallpaperView } from '../../hooks'
+import { SelectionScreenProps } from '../../navigation/types'
+import { hp, wp } from '../../utilities'
 
 const Selection: React.FC<SelectionScreenProps> = function (props) {
-  const { group: type, groupId: selectorId } = props.route.params;
-  const { wallpaper, setWallpaper } = useWallpaperView();
+  const { group: type, groupId: selectorId } = props.route.params
+  const { wallpaper, setWallpaper } = useWallpaperView()
   const variables = {
     bundleId: type === 'bundle' ? selectorId : '',
     categoryId: type === 'category' ? selectorId : '',
     colorId: type === 'color' ? selectorId : '',
-    tagsId: type === 'tag' ? selectorId : '',
-  };
+    tagsId: type === 'tag' ? selectorId : ''
+  }
 
   const { loading, data } = useWallpapersQuery({
-    variables: type === 'none' ? {} : variables,
-  });
+    variables: type === 'none' ? {} : variables
+  })
 
-  const { themedStyles } = useTheme();
+  const { themedStyles } = useTheme()
 
-  const renderDisplay = () => {
+  const renderDisplay = useCallback(() => {
     if (loading || !data) {
-      return <LoadingView height={88} />;
+      return <LoadingView height={88} />
     }
 
     return (
       <Cards
-        group="category"
+        HeaderComponent={<StackHeader title={props.route.params.title} />}
+        group='category'
         onClick={setWallpaper}
         items={data?.wallpapers as Wallpaper[]}
-        height="39"
-        width="47"
+        height='39'
+        width='47'
         disableLastMargin
         style={styles.cards}
       />
-    );
-  };
+    )
+  }, [data?.wallpapers, loading])
 
   return (
     <View style={[styles.root, themedStyles.bg]}>
-      <StackHeader title={props.route.params.title} />
+      {loading && <StackHeader title={props.route.params.title} />}
       {renderDisplay()}
-      <WallpaperView
-        wallpaper={wallpaper}
-        onCloseClick={() => setWallpaper(null)}
-      />
+      <WallpaperView wallpaper={wallpaper} onCloseClick={() => setWallpaper(null)} />
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'red'
   },
   displaySelection: {
     display: 'flex',
     justifyContent: 'space-around',
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   displayLayout: {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   modeIcon: {
-    paddingLeft: wp(4),
+    paddingLeft: wp(4)
   },
   gridView: {
     display: 'flex',
     flexWrap: 'wrap',
-    flexDirection: 'row',
+    flexDirection: 'row'
   },
   cards: {
-    marginBottom: wp(2),
+    marginBottom: wp(2)
   },
   sortOptions: {
     display: 'flex',
     justifyContent: 'center',
     flexDirection: 'row',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   downArrowIcon: {
     marginLeft: wp(2),
     height: hp(3),
-    width: wp(5),
+    width: wp(5)
   },
   sortingText: {
-    fontSize: wp(4),
+    fontSize: wp(4)
   },
   carousel: {
-    marginTop: hp(2.8),
-  },
-});
+    marginTop: hp(2.8)
+  }
+})
 
-export { Selection };
+export { Selection }
