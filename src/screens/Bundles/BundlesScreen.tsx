ca7f3c453189c92afa7a9ee6f973f1d96a94ccaa
WyvernDrexx <lilaprashadsharma@gmail.com>
2021-12-11 20:12:24 +0530

    [#180607639] bundles list header scroll

--- src/screens/Bundles/BundlesScreen.tsx
+++ src/screens/Bundles/BundlesScreen.tsx
@@ -1,57 +1,56 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Bundles } from '../../components/Carousel';
-import MiniSearchButton from '../../components/SearchBar/MiniSearchButton';
-import StackHeader from '../../components/StackHeader';
-import { Bundle, useBundlesScreenQuery } from '../../generated/graphql';
-import { useTheme } from '../../hooks';
-import { BundlesScreenProps } from '../../navigation/types';
-import { ItemGroup } from '../../types';
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Bundles } from '../../components/Carousel'
+import MiniSearchButton from '../../components/SearchBar/MiniSearchButton'
+import StackHeader from '../../components/StackHeader'
+import { Bundle, useBundlesScreenQuery } from '../../generated/graphql'
+import { useTheme } from '../../hooks'
+import { BundlesScreenProps } from '../../navigation/types'
+import { ItemGroup } from '../../types'
 
 const BundlesScreen: React.FC<BundlesScreenProps> = function (props) {
-  const { themedStyles } = useTheme();
-  const { data } = useBundlesScreenQuery();
+  const { themedStyles } = useTheme()
+  const { data } = useBundlesScreenQuery()
 
   const handleBundleClick = (bundle: Bundle, group: ItemGroup) => {
     props.navigation.navigate('Selection', {
       title: bundle.name,
       group,
-      groupId: bundle.id,
-    });
-  };
+      groupId: bundle.id
+    })
+  }
 
   const handleSearchButtonClick = () => {
-    props.navigation.navigate('Search');
-  };
+    props.navigation.navigate('Search')
+  }
 
   return (
     <View style={[styles.root, themedStyles.bg]}>
-      <StackHeader
-        right={<MiniSearchButton onClick={handleSearchButtonClick} />}
-        title="Bundles"
-      />
       <View>
         <Bundles
+          HeaderComponent={
+            <StackHeader right={<MiniSearchButton onClick={handleSearchButtonClick} />} title='Bundles' />
+          }
           loaderLight
           onClick={handleBundleClick}
           vertical
           items={data?.bundles as Bundle[]}
-          itemType="bundle"
-          width="30.7"
-          height="15"
+          itemType='bundle'
+          width='30.7'
+          height='15'
         />
       </View>
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   root: {
-    flex: 1,
+    flex: 1
   },
   container: {
-    justifyContent: 'flex-start',
-  },
-});
+    justifyContent: 'flex-start'
+  }
+})
 
-export { BundlesScreen };
+export { BundlesScreen }
